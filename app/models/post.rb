a185f17e54ce049aad4ab42f4d991998214e37f7
Craigton Lian <craigton.lian@gmail.com>
2023-01-14 18:28:58 +0800

    added post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   belongs_to :tag
   has_many :comments
 
-  validates :title, presence: true, length: { minimum: 2 }
-  validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
+  validates :title, presence: true, length: { minimum: 8 }
+  validates :body, presence: true, length: { minimum: 8, maximum: 500 }
   validates :user_id, presence: true
   validates :tag_id, presence: true
 end
